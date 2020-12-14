3548d46f9de0c549c08d6d601825baac10232d47
Sergey <sergey@Sergeys-iMac.local>
2020-12-14 12:39:11 +0200

    For gh-pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import HomePage from './components/HomePage/HomePage';
 import Login from './components/Login/Login';
@@ -6,15 +5,7 @@ import Login from './components/Login/Login';
 function App() {
   return (
     <div className='App'>
-      <nav>
-        <Link className='login-link' to='/login'>
-          Login
-        </Link>
-      </nav>
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/login' component={Login} />
-      </Switch>
+      <HomePage />
     </div>
   );
 }
